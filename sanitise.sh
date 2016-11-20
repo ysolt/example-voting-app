74c34fdb56f52f1683546500fd6acb9dc99544c9
Zsolt Molnar <ysolt@ysolt.net>
2016-11-20 17:31:05 +0100

    fix: use proper username in sanitise script

--- sanitise.sh
+++ sanitise.sh
@@ -1,7 +1,7 @@
 sudo rm -fr /*/vagrant/.ssh /root/.ssh 
 
-sudo passwd --delete lbs
-sudo passwd -l lbs
+sudo passwd --delete vagrant
+sudo passwd -l vagrant
 
 sudo passwd --delete root
 sudo passwd -l root
